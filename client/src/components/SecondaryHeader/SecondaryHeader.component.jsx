2f57b360ba467f6d30e4d480b9c4fce85891a9e5
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2020-04-05 15:35:21 +0530

    secondat header

--- client/src/components/SecondaryHeader/SecondaryHeader.component.jsx
+++ client/src/components/SecondaryHeader/SecondaryHeader.component.jsx
@@ -4,7 +4,26 @@ class SecondaryHeader extends Component {
     render() {
         return (
             <div>
-
+                <ul class="nav justify-content-center nav-fill bg-light border border-dark">
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Automobiles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Real Estate</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Electronics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Fashion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Furniture</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-body" href="#">Pets</a>
+                    </li>
+                </ul>
             </div>
         )
     }
--- client/src/pages/HomePage/Homepage.jsx
+++ client/src/pages/HomePage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainHeader from "../../components/MainHeader/MainHeader.component"
+import SecondaryHeader from "../../components/SecondaryHeader/SecondaryHeader.component"
 class HomePage extends React.Component {
     render() {
         return (
             <div>
                 <MainHeader />
+                <SecondaryHeader />
             </div>
         );
     }
